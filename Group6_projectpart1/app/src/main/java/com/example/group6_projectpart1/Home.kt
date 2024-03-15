dce81593c8a145b6a9cbbafaf702c3406a118371
Roshan <roshanpeter26@hotmail.com>
2024-03-14 23:02:59 -0400

    changes made

--- Group6_projectpart1/app/src/main/java/com/example/group6_projectpart1/Home.kt
+++ Group6_projectpart1/app/src/main/java/com/example/group6_projectpart1/Home.kt
@@ -2,10 +2,27 @@ package com.example.group6_projectpart1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.Job
 
 class Home : AppCompatActivity() {
+    private var adapter: MainAdapter? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val mainRecycler: RecyclerView = findViewById(R.id.mainRecycler)
+
+        //update path
+        val query = FirebaseDatabase.getInstance().reference.child("products")
+        val options = FirebaseRecyclerOptions.Builder<job>().setQuery(query,job::class.java).build()
+        adapter = MainAdapter(options)
+
+        mainRecycler.layoutManager = LinearLayoutManager(this)
+        mainRecycler.adapter = adapter;
+
     }
 }
---
+++ Group6_projectpart1/app/src/main/java/com/example/group6_projectpart1/job.kt
@@ -0,0 +1,3 @@
+package com.example.group6_projectpart1
+
+class job (var Title:String = "",var name:String = "",var id:String = "",var image:String = "")
